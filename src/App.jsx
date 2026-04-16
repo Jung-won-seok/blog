b0129559e1f2bc25caf8cd090c61deb363ef6582
JungWonSeok97 <ukksj0621@enu-tech.co.kr>
2026-04-16 13:01:40 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
+/*eslint-disable*/
 
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let post = "강남 우동 맛집";
+  let [글제목, b] = useState(["남자코트 추천", "강남 우동맛집", "파이썬 독학"]);
+  let [따봉, 따봉변경] = useState(0);
 
-let post = '강남 우동 맛집';
-// document.querySelector('h4').innerHTML = post;
- 
+  function 함수() {
+    console.log(1);
+  }
+  // document.querySelector('h4').innerHTML = post;
+
   return (
-    <div className='App'>
+    <div className="App">
       <div className="black-nav">
-        <h4 style={{color : 'red', fontSize : '16px'}}>블로그임</h4>
+        <h4>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {따봉}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
       </div>
       <h4>{post}</h4>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
