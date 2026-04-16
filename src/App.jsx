e0ebbea4b5454658fda3b68c2bd699a963f3ec42
Jung-won-seok <ukksj0621@naver.com>
2026-04-16 12:36:33 +0900

    Update App.jsx and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+let post = '강남 우동 맛집';
+// document.querySelector('h4').innerHTML = post;
  
   return (
-    <div></div>
+    <div className='App'>
+      <div className="black-nav">
+        <h4 style={{color : 'red', fontSize : '16px'}}>블로그임</h4>
+      </div>
+      <h4>{post}</h4>
+    </div>
   )
 }
 
